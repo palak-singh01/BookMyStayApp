58fc63a1dbf2021dd60c464feeb7ef54185eeaab
palak-singh01 <palaksingh35584@gmail.com>
2026-03-23 19:09:52 +0530

    Application Entry & Welcome Message

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,14 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        // Step 1: Program starts (JVM calls main method)
+
+        // Step 2: Display welcome message with app name and version
+        System.out.println("=======================================");
+        System.out.println(" Welcome to Hotel Booking Application ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("=======================================");
+
+        // Step 3: Application terminates automatically after execution
+    }
+}
+
